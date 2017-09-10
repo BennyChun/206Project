9d1775e7828ed4fababcc1f5f331deed711b0c8e
Benny Chun <bchu352@aucklanduni.ac.nz>
2017-09-11 01:21:57 +1200

    StartMenu controller skeleton made

--- src/application/view/StartMenuController.java
+++ src/application/view/StartMenuController.java
@@ -1,5 +1,20 @@
 package application.view;
 
+import javafx.stage.Stage;
+
 public class StartMenuController {
+	
+	
+	public void handlePlay() {
+		
+	}
+	
+	public void handleStats() {
+		
+	}
+
+	public void handleQuit() {
+		
+	}	
 
 }
