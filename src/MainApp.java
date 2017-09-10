9595527d526fcb7ec64c7afed7c9da7e7133f921
Benny Chun <bchu352@aucklanduni.ac.nz>
2017-09-10 22:33:05 +1200

    removed rootlayout, added fields to MainApp

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,12 +1,16 @@
 
 import javafx.application.Application;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
+	private Stage _primaryStage;
+	private BorderPane _startMenu;
+	
 	@Override
 	public void start(Stage primaryStage) {
-		
+	
 	}
 
 	public static void main(String[] args) {
