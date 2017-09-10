14a5eb13e516d13c91166d6d4f70bd0d9fb23cc0
Benny Chun <bchu352@aucklanduni.ac.nz>
2017-09-10 22:49:28 +1200

    Got start menu to pop up

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,5 +1,9 @@
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -10,9 +14,29 @@ public class MainApp extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) {
-	
+		_primaryStage = primaryStage;
+		_primaryStage.setTitle("Tatai!");
+		
+		initStartMenu();
 	}
 
+	public void initStartMenu(){
+		try {
+			//load start menu from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("application/view/StartMenu.fxml"));
+			_startMenu = (BorderPane) loader.load();
+			
+			// Show the scene containing the start menu
+			Scene scene = new Scene(_startMenu);
+			_primaryStage.setScene(scene);
+			_primaryStage.show();
+			_primaryStage.setResizable(false);
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
