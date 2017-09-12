4b3ec4ded6e9a8cdb9eac9735a297262b384b7f0
Benny Chun <bchu352@aucklanduni.ac.nz>
2017-09-13 02:11:58 +1200

    added stats and confirm answer screen fxmls, and added LevelScore class with fields

---
+++ src/application/model/LevelScore.java
@@ -0,0 +1,14 @@
+package application.model;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class LevelScore {
+	
+	private String _level;
+	private int _score;
+	private int _attempts;
+	private Date _timeStamp;
+	private Time _timeElasped;
+	
+}
