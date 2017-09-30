fc68ff4a7b96e6631f525c4f527f467a0bc583e1
Benny Chun <bennychun97@gmail.com>
2017-09-30 21:56:58 +1300

    Methods for endScreen controller implemented

--- src/application/view/EndScreenController.java
+++ src/application/view/EndScreenController.java
@@ -6,17 +6,17 @@ public class EndScreenController extends AbstractController{
 	
 	@FXML
 	public void handleMainMenu() {
-		
+		_mainApp.initStartMenu();
 	}
 	
 	@FXML
 	public void handleHard() {
-		
+		_mainApp.initLevelScreen("hard");
 	}
 	
 	@FXML 
 	public void handleRetry() {
-		
+		_mainApp.initLevelScreen("easy");
 	}
 
 }
