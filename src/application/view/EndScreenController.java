184582186fc3e1ac7a1aa318295692758722ba4b
Pathical <pathicalmine@gmail.com>
2017-10-01 15:09:40 +1300

    Fixed merge conflict in bringing a03Master up to date.

--- src/application/view/EndScreenController.java
+++ src/application/view/EndScreenController.java
@@ -24,34 +24,32 @@ public class EndScreenController extends AbstractController{
 	public void handleHard() {
 		_mainApp.initLevelScreen("hard");
 	}
-	
+
 	@FXML 
 	public void handleRetry() {
-<<<<<<< HEAD
-		_mainApp.initLevelScreen("easy");
-=======
+
 		if (previousLevel.equals("hard")) {
 			_mainApp.initLevelScreen("hard");
 		}else {
 			_mainApp.initLevelScreen("easy");
 		}
 	}
-	
+
 	public void setPreviousLevel(String previousLevel) {
 		this.previousLevel = previousLevel;
 	}
-	
+
 	public void setScoreLabel(int finalScore) {
 		score.setText(Integer.toString(finalScore)+"/10");
 	}
-	
+
 	public void disableHardButton(int finalScore) {
 		if (finalScore >=8) {
 			hard.setDisable(false);
 		}else {
 			hard.setDisable(true);
 		}
->>>>>>> 058bb7bedddf58b13fd4e5a3fb13d39059bae1f1
+
 	}
 
 }
