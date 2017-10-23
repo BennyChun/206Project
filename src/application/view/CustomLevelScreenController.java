6e31512c859ace18608dc2385b9c885c03a4ce8f
Benny Chun <bennychun97@gmail.com>
2017-10-23 22:09:35 +1300

    linked custome level select icon update

--- src/application/view/CustomLevelScreenController.java
+++ src/application/view/CustomLevelScreenController.java
@@ -1,8 +1,20 @@
 package application.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+import static application.MainApp.mascotImage;
 
 public class CustomLevelScreenController extends AbstractController{
+
+    @FXML private ImageView mascot;
+
+    @FXML
+    private void initialize(){
+
+        mascot.setImage(mascotImage);
+    }
+
 
     @FXML
     public void handleBack(){
