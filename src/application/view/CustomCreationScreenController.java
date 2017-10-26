677d502f3424ce445e5b021439b951f63279b12f
Benny Chun <bennychun97@gmail.com>
2017-10-26 21:15:37 +1300

    created fields and methods for custom creation

--- src/application/view/CustomCreationScreenController.java
+++ src/application/view/CustomCreationScreenController.java
@@ -1,16 +1,35 @@
 package application.view;
 
+import application.model.EquationQuestion;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class CustomCreationScreenController extends AbstractController {
 
-    @FXML private TableV;
-    @FXML private ;
-    @FXML private ;
-    @FXML private ;
-    @FXML private ;
-    @FXML private ;
-    @FXML private ;
-    @FXML private ;
-    @FXML private ;
-    @FXML private ;
-    @FXML private ;
+    @FXML private TableView customLevelTable;
+    @FXML private TableColumn<String, EquationQuestion> equationListNameCol;
+    @FXML private TableColumn<String, EquationQuestion> equationCreationDateCol;
+    @FXML private Button addBtn;
+    @FXML private Button subBtn;
+    @FXML private Button mulBtn;
+    @FXML private Button divBtn;
+    @FXML private TextField boundaryValue;
+    @FXML private TextField equationListName;
+    @FXML private Button generateButton;
+
+    @FXML
+    private void handleBack(){
+        _mainApp.initCustom();
+    }
+    @FXML
+    private void generate(){
+
+
+        _mainApp.initCustom();
+    }
+
+    
 }
