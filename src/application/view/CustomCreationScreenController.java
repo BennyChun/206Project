db3f821fc2e9a4d8266e0d63c1e52deb10d4bea7
Pathical <pathicalmine@gmail.com>
2017-10-27 01:33:46 +1300

    on the CustomScreationScreenController, it now displays a dialog if the user did not proved valid input(s).

--- src/application/view/CustomCreationScreenController.java
+++ src/application/view/CustomCreationScreenController.java
@@ -34,11 +34,69 @@ public class CustomCreationScreenController extends AbstractController {
         //then check if the user entered a number (boundaryValue) if between 10 and 99 (if we allow them to set the upper bound to be like 1, that'll be stupid)
         //then get the equationListName , set as a string
 
+        String errorMessage = "";
+
         ArrayList<String> selectedOperations = getTickBoxes();
+        if (selectedOperations.size() == 0){
+            //the user did not select a operation
+            errorMessage+= "You did not select an operation, please select one\n";
+        }
+
+        //----------------------------------------------------------------------
+        String usersBoundary = boundaryValue.getText();
+        String digitRegex = "\\d+";
+        int result;
+        if (usersBoundary.length()==0){//if the user did not enter anything for the upper bound
+            errorMessage+= "You did not enter a number for the max number\n";
+        }
+        if (usersBoundary.matches(digitRegex)){
+            result = Integer.parseInt(usersBoundary);//converts the users answer to an integer
+            //will throw an exception if it cannot parse the string to an integer
+            if (result <10 || result >99){
+                errorMessage+="Please enter a integer between 10 and 99\n";
+            }
+
+        }else {
+            //the user did not enter a digit
+            errorMessage+="You need to enter a integer !\n";
+        }
+        //--------------------------------------------
+
+        String usersName = equationListName.getText();
+        if (usersName.length() == 0){
+            errorMessage+= "Please enter a name for this custom game \n";
+        }
 
 
-        _mainApp.initCustom();
+
+        if (errorMessage.length() == 0) {
+            //if the user has no error messages
+
+            //create random equation questions
+
+
+
+
+            //go back to custom screen (the screen that was a TableView of all the users custom games
+            //and it lets the user select one to play or create one.
+            _mainApp.initCustom();
+        } else {
+            // Show the error message.
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Invalid Fields");
+            alert.setHeaderText("Please correct invalid fields");
+            alert.setContentText(errorMessage);
+
+            alert.showAndWait();
+
+            return ;
+        }
+
+
+
     }
+
+
 
     /**
      * this method will get the selected tick boxes
